c97ee63baa6660a3c89910df0c4cdf0072a903c9
CrugXC <jtrixie@NN123-HA-17.HS.sd203.org>
2015-08-20 15:07:49 -0500

    I did stuff.

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,22 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setBodyColor(Color.PINK);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turnLeft();
+        turtle.forward(80);
+        turtle.setPenColor(Color.GREEN);
+        turtle.turnRight();
+        turtle.forward(100);
+        turtle.turn(180);
+        turtle.setPenColor(Color.BLUE);
+        turtle.forward(200);
+        turtle.penUp();
+        turtle.turnLeft();
+        turtle.forward(100);
+        turtle.setPenWidth(17490283);
+        turtle.penDown();
+        turtle.forward();
     }
 }
