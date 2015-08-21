6fe5e83f5eb207f47ece615db604b9c8a4a9332b
CrugXC <jtrixie@NN123-HA-17.HS.sd203.org>
2015-08-21 15:07:14 -0500

    Created While Loop

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,25 +3,20 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
+    throws InterruptedException
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setBodyColor(Color.PINK);
         turtle.penDown();
-        turtle.forward(50);
-        turtle.turnLeft();
-        turtle.forward(80);
-        turtle.setPenColor(Color.GREEN);
-        turtle.turnRight();
-        turtle.forward(100);
-        turtle.turn(180);
-        turtle.setPenColor(Color.BLUE);
-        turtle.forward(200);
-        turtle.penUp();
-        turtle.turnLeft();
-        turtle.forward(100);
-        turtle.setPenWidth(17490283);
-        turtle.penDown();
-        turtle.forward();
+        Thread.sleep(1000);
+        
+        while(1 == 1)
+        {
+            turtle.forward(3);
+            turtle.turn(1);
+            Thread.sleep(10);
+          
+        }
     }
 }
